2dc1a7eaffe35818ad1b05480f2e0f956b80c954
Josh Dewhurst <108956371+joshdewhurst@users.noreply.github.com>
2023-04-24 16:35:07 -0700

    Update main.py
    updating a guess to the word

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ words = ['python', 'programming', 'computer', 'data', 'science', 'california', '
 # Function to choose a random word from the list
 def choose_word():
     return random.choice(words)
+
+# Function to update the word with correctly guessed letters
+def update_word(word, guess, letter):
+    for i in range(len(word)):
+        if word[i] == letter:
+            guess = guess[:i] + letter + guess[i+1:]
+    return guess
