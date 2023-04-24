6e5b49ae9817dc4727f9bea48edabe1ba751d0cc
Josh Dewhurst <108956371+joshdewhurst@users.noreply.github.com>
2023-04-24 16:34:08 -0700

    Update main.py
    adding a function to choose a word

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import random
 # List of words for the game
 words = ['python', 'programming', 'computer', 'data', 'science', 'california', 'april', 'software']
 
+# Function to choose a random word from the list
+def choose_word():
+    return random.choice(words)
